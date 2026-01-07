27747a47a53b132cb545b496c2936a6e80cbcedc
Joe Spagnuolo <joe@spagnuolo.biz>
2026-01-07 18:14:58 -0500

    don't fade out text on last frame

--- script.js
+++ script.js
@@ -51,8 +51,12 @@ sections.forEach((section, index) => {
         { y: 60, opacity: 0, scale: 0.95 },
         { y: 0, opacity: 1, scale: 1, duration: 0.4, ease: "power2.out" }
     )
-        .to(content, { opacity: 1, duration: 0.2 }) // Hold position
-        .to(content, { y: -60, opacity: 0, scale: 1.05, duration: 0.4, ease: "power2.in" });
+        .to(content, { opacity: 1, duration: 0.2 }); // Hold position
+
+    // Only fade out if it's not the last section
+    if (index < sections.length - 1) {
+        tl.to(content, { y: -60, opacity: 0, scale: 1.05, duration: 0.4, ease: "power2.in" });
+    }
 
     // Background Image Animation (Parallax + Color/Scale)
     // This happens over the entire scroll duration of the section
